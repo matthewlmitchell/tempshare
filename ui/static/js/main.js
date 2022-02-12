772961f673e105fea0121236b026aeaa4dd5a9e5
Matthew Mitchell <8042849+matthewlmitchell@users.noreply.github.com>
2022-02-11 21:59:32 -0600

    Added dark mode persistence

--- ui/static/js/main.js
+++ ui/static/js/main.js
@@ -9,6 +9,17 @@ for (var i = 0; i < navLinks.length; i++) {
 
 const switchTheme = document.querySelector("#switch");
 
-switchTheme.addEventListener("click", function () {
-  document.body.classList.toggle("dark-mode");
+switchTheme.addEventListener("click", () => {
+	document.body.classList.toggle("dark-mode");
+
+	if(document.body.classList.contains("dark-mode")) {
+		localStorage.setItem("dark-mode", "enabled");
+	} else {
+		localStorage.setItem("dark-mode", "disabled");
+	}
 });
+
+if(localStorage.getItem("dark-mode") == "enabled") {
+	document.body.classList.toggle("dark-mode");
+	document.getElementById("switch").checked = true;
+}
