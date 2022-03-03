c764d2f4c1a0dc5aed4a5062a5debcce2afa166a
Matthew Mitchell <8042849+matthewlmitchell@users.noreply.github.com>
2022-03-02 22:48:03 -0600

    Added tests for /about and /view handlers

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -46,3 +46,45 @@ func TestCreateTempShareForm(t *testing.T) {
 	}
 
 }
+
+func TestViewTempShareForm(t *testing.T) {
+	app := newTestApplication(t)
+
+	testServ := newTestServer(t, app.routes(), false)
+	defer testServ.Close()
+
+	statusCode, _, responseBody := testServ.get(t, "/view")
+
+	if statusCode != http.StatusOK {
+		t.Errorf("Expected status %d, received %d", http.StatusOK, statusCode)
+	}
+
+	strResponse := string(responseBody)
+
+	expectedValue := "View - TempShare"
+	if !strings.Contains(strResponse, expectedValue) {
+		t.Errorf("Expected %s in response body, received %s", expectedValue, strResponse)
+	}
+}
+
+func TestAbout(t *testing.T) {
+	app := newTestApplication(t)
+
+	testServ := newTestServer(t, app.routes(), false)
+	defer testServ.Close()
+
+	statusCode, _, responseBody := testServ.get(t, "/about")
+
+	if statusCode != http.StatusOK {
+		t.Errorf("Expected status %d, received %d", http.StatusOK, statusCode)
+	}
+
+	strResponse := string(responseBody)
+
+	expectedValue := "About - TempShare"
+	if !strings.Contains(strResponse, expectedValue) {
+		t.Errorf("Expected %s in response body, received %s", expectedValue, strResponse)
+	}
+}
+
+// TODO: Add tests for form submission and spinning up a mock MySQL DB
