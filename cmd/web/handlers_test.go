ba70ea1f7ec485f730b1eade7cb73beab49868e4
Matthew Mitchell <8042849+matthewlmitchell@users.noreply.github.com>
2022-03-10 14:46:02 -0600

    Added tests for POST to /create

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -49,6 +49,116 @@ func TestCreateTempShareForm(t *testing.T) {
 
 }
 
+func TestCreateTempShare(t *testing.T) {
+	app := newTestApplication(t)
+
+	err := app.initializeClient("./../../tls/cert.pem")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	testServ := newTestServer(t, app.routes(), true)
+	defer testServ.Close()
+
+	_, _, responseBody := testServ.get(t, "/create")
+	csrfToken := extractCSRFToken(t, responseBody)
+
+	testCases := []struct {
+		name                    string
+		tokenCSRF               string
+		inputTempShareText      string
+		inputTempShareExpires   string
+		inputTempShareViewLimit string
+		expectedStatusCode      int
+		expectedResponse        []byte
+	}{
+		{
+			name:                    "Valid Submission",
+			tokenCSRF:               csrfToken,
+			inputTempShareText:      "Hello World",
+			inputTempShareExpires:   "1",
+			inputTempShareViewLimit: "1",
+			expectedStatusCode:      http.StatusOK,
+			expectedResponse:        []byte("Your TempShare link: https://"),
+		},
+		{
+			name:                    "Empty CSRF Token",
+			tokenCSRF:               "",
+			inputTempShareText:      "Hello World",
+			inputTempShareExpires:   "1",
+			inputTempShareViewLimit: "1",
+			expectedStatusCode:      http.StatusForbidden,
+			expectedResponse:        []byte("Forbidden - CSRF token not found in request"),
+		},
+		{
+			name:                    "Invalid CSRF Token",
+			tokenCSRF:               "INVALID",
+			inputTempShareText:      "Hello World",
+			inputTempShareExpires:   "1",
+			inputTempShareViewLimit: "1",
+			expectedStatusCode:      http.StatusForbidden,
+			expectedResponse:        []byte("Forbidden - CSRF token invalid"),
+		},
+		{
+			name:                    "Text too short",
+			tokenCSRF:               csrfToken,
+			inputTempShareText:      "A",
+			inputTempShareExpires:   "1",
+			inputTempShareViewLimit: "1",
+			expectedStatusCode:      http.StatusOK,
+			expectedResponse:        []byte("This field must contain more than 2 characters"),
+		},
+		{
+			name:                    "No input",
+			tokenCSRF:               csrfToken,
+			inputTempShareText:      "",
+			inputTempShareExpires:   "1",
+			inputTempShareViewLimit: "1",
+			expectedStatusCode:      http.StatusOK,
+			expectedResponse:        []byte("This field must not be blank"),
+		},
+		{
+			name:                    "Invalid expiry",
+			tokenCSRF:               csrfToken,
+			inputTempShareText:      "Hello World",
+			inputTempShareExpires:   "100",
+			inputTempShareViewLimit: "1",
+			expectedStatusCode:      http.StatusOK,
+			expectedResponse:        []byte("This field is invalid."),
+		},
+		{
+			name:                    "Invalid view limit",
+			tokenCSRF:               csrfToken,
+			inputTempShareText:      "Hello World",
+			inputTempShareExpires:   "1",
+			inputTempShareViewLimit: "100",
+			expectedStatusCode:      http.StatusOK,
+			expectedResponse:        []byte("This field is invalid."),
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			form := url.Values{}
+			form.Add("gorilla.csrf.Token", testCase.tokenCSRF)
+			form.Add("text", testCase.inputTempShareText)
+			form.Add("expires", testCase.inputTempShareExpires)
+			form.Add("viewlimit", testCase.inputTempShareViewLimit)
+			form.Add("g-recaptcha-response", "this-value-doesnt-matter-for-test-servers")
+
+			statusCode, _, responseBody := testServ.postForm(t, "/create", form)
+
+			if statusCode != testCase.expectedStatusCode {
+				t.Errorf("Expected status %d, received status %d", testCase.expectedStatusCode, statusCode)
+			}
+
+			if !bytes.Contains(responseBody, testCase.expectedResponse) {
+				t.Errorf("Expected %s, received %s", string(testCase.expectedResponse), string(responseBody))
+			}
+		})
+	}
+}
+
 func TestViewTempShareForm(t *testing.T) {
 	app := newTestApplication(t)
 
@@ -104,11 +214,18 @@ func TestViewTempShare(t *testing.T) {
 			expectedResponse:   []byte("This link has 0 uses remaining."),
 		},
 		{
-			name:               "Invalid CSRF token",
+			name:               "Empty CSRF Token",
 			tokenCSRF:          "",
 			tokenTempShare:     "MUPPH5PDKV7AGCUAAEERL5ARIXICVVGYLRIV365X5XSV3EKISAXQ",
 			expectedStatusCode: http.StatusForbidden,
 			expectedResponse:   []byte("Forbidden - CSRF token not found in request"),
+		},
+		{
+			name:               "Invalid CSRF Token",
+			tokenCSRF:          "INVALID",
+			tokenTempShare:     "MUPPH5PDKV7AGCUAAEERL5ARIXICVVGYLRIV365X5XSV3EKISAXQ",
+			expectedStatusCode: http.StatusForbidden,
+			expectedResponse:   []byte("Forbidden - CSRF token invalid"),
 		},
 		{
 			name:               "Invalid TempShare token",
